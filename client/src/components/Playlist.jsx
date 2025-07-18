96ac2422b837ad27d1c23f119eb82df88ae6a8c7
Shubhamcse08 <shubhamcse.eng@gmail.com>
2025-07-18 15:11:25 +0530

    Update Playlist.jsx

--- client/src/components/Playlist.jsx
+++ client/src/components/Playlist.jsx
@@ -1,6 +1,6 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import "./Playlist.css";
+import "./playlist.css";
 import project from "./data";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
